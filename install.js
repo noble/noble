ab453fa7162b5ab34e9d0c335ef437814d686263
Sandeep Mistry <sandeep.mistry@gmail.com>
2013-04-13 16:27:28 -0400

    - install script

---
+++ install.js
@@ -0,0 +1,18 @@
+var os = require('os');
+var exec = require('child_process').exec;
+
+var platform = os.platform();
+
+console.log('noble install: platform is "' + platform + "'");
+
+if (platform === 'darwin') {
+  console.log('noble install: installing xpc-connection ...');
+
+  exec('npm install xpc-connection@0.0.1', function(error, stdout, stderr) {
+    console.log('noble install: done');
+    process.exit(error ? -1 : 0);
+  });
+} else {
+  console.error('noble install: Your platform is not supported!');
+  process.exit(-1);
+}
