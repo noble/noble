420d99d47398bb25be1c5ba34a22d5298955818b
Sandeep Mistry <sandeep.mistry@gmail.com>
2013-12-11 10:40:53 -0500

    - also print out service data, manufacturer data, tx power level if available

--- examples/advertisement-discovery.js
+++ examples/advertisement-discovery.js
@@ -14,6 +14,19 @@ noble.on('discover', function(peripheral) {
   console.log('\t\t' + peripheral.advertisement.localName);
   console.log('\tcan I interest you in any of the following advertised services:');
   console.log('\t\t' + JSON.stringify(peripheral.advertisement.serviceUuids));
+  if (peripheral.advertisement.serviceData) {
+    console.log('\there is my service data:');
+    console.log('\t\t' + JSON.stringify(peripheral.advertisement.serviceData.toString('hex')));
+  }
+  if (peripheral.advertisement.manufacturerData) {
+    console.log('\there is my manufacturer data:');
+    console.log('\t\t' + JSON.stringify(peripheral.advertisement.manufacturerData.toString('hex')));
+  }
+  if (peripheral.advertisement.txPowerLevel) {
+    console.log('\tmy TX power level is:');
+    console.log('\t\t' + peripheral.advertisement.txPowerLevel);
+  }
+
   console.log();
 });
 
