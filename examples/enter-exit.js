a12415c84585fbb9626e1571f1312d007d5e1469
Sandeep Mistry <sandeep.mistry@gmail.com>
2015-02-26 20:50:06 -0500

    - initial enter-exit example
    based on code provided by Mattias Ask

---
+++ examples/enter-exit.js
@@ -0,0 +1,48 @@
+/*
+  Continously scans for peripherals and prints out message when they enter/exit
+
+    In range criteria:      RSSI < threshold
+    Out of range criteria:  lastSeen > grace period
+
+  based on code provided by: Mattias Ask
+*/
+var noble = require('../index');
+
+var RSSI_THRESHOLD    = -90;
+var EXIT_GRACE_PERIOD = 2000; // milliseconds
+
+var inRange = [];
+
+noble.on('discover', function(peripheral) {
+  if (peripheral.rssi < RSSI_THRESHOLD) {
+    // ignore
+    return;
+  }
+
+  var uuid = peripheral.uuid;
+  var entered = !inRange[uuid];
+
+  if (entered) {
+    inRange[uuid] = {
+      peripheral: peripheral
+    };
+
+    console.log('"' + peripheral.advertisement.localName + '" entered (RSSI ' + peripheral.rssi + ') ' + new Date());
+  }
+
+  inRange[uuid].lastSeen = Date.now();
+});
+
+setInterval(function() {
+  for (var uuid in inRange) {
+    if (inRange[uuid].lastSeen < (Date.now() - EXIT_GRACE_PERIOD)) {
+      var peripheral = inRange[uuid].peripheral;
+
+      console.log('"' + peripheral.advertisement.localName + '" exited (RSSI ' + peripheral.rssi + ') ' + new Date());
+
+      delete inRange[uuid];
+    }
+  }
+}, EXIT_GRACE_PERIOD / 2);
+
+noble.startScanning([], true);
