bc30656f51298f6c407a36f8c3f3c1e0af317a18
Jacob Rosenthal <jakerosenthal@gmail.com>
2016-07-19 18:22:25 -0700

    bring tests up to abstract level

---
+++ test/test-bindings-abstract.js
@@ -0,0 +1,258 @@
+var should = require('should');
+var sinon = require('sinon');
+
+var a = require('./common');
+
+var startScanningEmitScanStart = function(bindings, Native, setup) {
+  describe('Noble bindings central', function() {
+    var sandbox = sinon.sandbox.create();
+    var mock;
+
+    beforeEach(function() {
+      mock = new Native(bindings, sandbox);
+    });
+
+    afterEach(function () {
+      sandbox.restore();
+      mock = null;
+    });
+
+    it('startScanning should emit scanStart', function() {
+      var eventSpy = sandbox.spy();
+      bindings.on('scanStart', eventSpy);
+
+      if (typeof setup == 'function')
+        setup(mock, sandbox);
+
+      bindings.startScanning();
+
+      eventSpy.called.should.equal(true);
+    });
+
+  });
+};
+
+var stopScanningEmitScanStop = function(bindings, Native, setup) {
+  describe('Noble bindings central', function() {
+    var sandbox = sinon.sandbox.create();
+    var mock;
+
+    beforeEach(function() {
+      mock = new Native(bindings, sandbox);
+    });
+
+    afterEach(function () {
+      sandbox.restore();
+      mock = null;
+    });
+
+    it('stopScanning should emit scanStop', function() {
+      var eventSpy = sandbox.spy();
+      bindings.on('scanStop', eventSpy);
+
+      if (typeof setup == 'function')
+        setup(mock, sandbox);
+
+      bindings.stopScanning();
+
+      eventSpy.called.should.equal(true);
+    });
+  });
+};
+
+var emitAddressChange = function(bindings, Native, setup) {
+
+  describe('Noble bindings central', function() {
+    var sandbox = sinon.sandbox.create();
+    var mock;
+
+    beforeEach(function() {
+      mock = new Native(bindings, sandbox);
+    });
+
+    afterEach(function () {
+      sandbox.restore();
+      mock = null;
+    });
+
+    it('should emit addressChange', function() {
+      var eventSpy = sandbox.spy();
+      bindings.once('addressChange', eventSpy);
+
+      if (typeof setup == 'function')
+        setup(mock, sandbox);
+
+      eventSpy.calledWithExactly(a.addressString).should.equal(true);
+    });
+
+  });
+
+};
+
+
+var emitStateChange = function(bindings, Native, setup) {
+
+  describe('Noble bindings central', function() {
+    var sandbox = sinon.sandbox.create();
+    var mock;
+
+    beforeEach(function() {
+      mock = new Native(bindings, sandbox);
+    });
+
+    afterEach(function () {
+      sandbox.restore();
+      mock = null;
+    });
+
+    it('should emit stateChange', function() {
+      var eventSpy = sandbox.spy();
+      bindings.once('stateChange', eventSpy);
+
+      if (typeof setup == 'function')
+        setup(mock, sandbox);
+
+      eventSpy.calledWithExactly(a.stateString).should.equal(true);
+    });
+
+  });
+
+};
+
+
+
+var emitDiscover = function(bindings, Native, setup) {
+
+  describe('Noble bindings central', function() {
+    var sandbox = sinon.sandbox.create();
+    var mock;
+
+    beforeEach(function() {
+      mock = new Native(bindings, sandbox);
+    });
+
+    afterEach(function () {
+      sandbox.restore();
+      mock = null;
+    });
+
+    it('should emit discover', function() {
+      var eventSpy = sandbox.spy();
+      bindings.once('discover', eventSpy);
+
+      if (typeof setup == 'function')
+        setup(mock, sandbox);
+
+      eventSpy.calledWithExactly(a.peripheralUuidString, a.addressString, a.addressTypeString, a.connectableBoolean, a.advertisementObject, a.rssiNumber).should.equal(true);
+    });
+
+  });
+
+};
+
+
+
+var emitConnectSuccess = function(bindings, Native, setup) {
+
+  describe('Noble bindings central', function() {
+    var sandbox = sinon.sandbox.create();
+    var mock;
+
+    beforeEach(function() {
+      mock = new Native(bindings, sandbox);
+    });
+
+    afterEach(function () {
+      sandbox.restore();
+      mock = null;
+    });
+
+    it('should emit connect on success', function() {
+      var eventSpy = sandbox.spy();
+      bindings.once('connect', eventSpy);
+
+      if (typeof setup == 'function')
+        setup(mock, sandbox);
+
+      eventSpy.calledWithExactly(a.peripheralUuidString, a.mockError).should.equal(true);
+    });
+
+  });
+
+};
+
+
+
+var emitConnectFail = function(bindings, Native, setup) {
+
+  describe('Noble bindings central', function() {
+    var sandbox = sinon.sandbox.create();
+    var mock;
+
+    beforeEach(function() {
+      mock = new Native(bindings, sandbox);
+    });
+
+    afterEach(function () {
+      sandbox.restore();
+      mock = null;
+    });
+
+    it('should emit connect on fail', function() {
+      var eventSpy = sandbox.spy();
+      bindings.once('connect', eventSpy);
+
+      if (typeof setup == 'function')
+        setup(mock, sandbox);
+
+      eventSpy.calledWithExactly(a.peripheralUuidString, a.mockError).should.equal(true);
+    });
+
+  });
+
+};
+
+
+
+
+var emitDisconnect = function(bindings, Native, setup) {
+
+  describe('Noble bindings central', function() {
+    var sandbox = sinon.sandbox.create();
+    var mock;
+
+    beforeEach(function() {
+      mock = new Native(bindings, sandbox);
+    });
+
+    afterEach(function () {
+      sandbox.restore();
+      mock = null;
+    });
+
+    it('should emit disconnect', function() {
+      var eventSpy = sandbox.spy();
+      bindings.once('disconnect', eventSpy);
+
+      if (typeof setup == 'function')
+        setup(mock, sandbox);
+
+      bindings.disconnect(a.peripheralUuidString);
+      eventSpy.calledWithExactly(a.peripheralUuidString).should.equal(true);
+    });
+
+  });
+
+};
+
+
+module.exports = {
+  startScanningEmitScanStart: startScanningEmitScanStart,
+  stopScanningEmitScanStop: stopScanningEmitScanStop,
+  emitAddressChange: emitAddressChange,
+  emitStateChange: emitStateChange,
+  emitDiscover: emitDiscover,
+  emitConnectSuccess: emitConnectSuccess,
+  emitConnectFail: emitConnectFail,
+  emitDisconnect: emitDisconnect
+};
--- test/test-bindings-central.js
+++ test/test-bindings-central.js
@@ -6,12 +6,61 @@ var Mock = require('./core-bluetooth-mock');
 
 var a = require('./common');
 
-describe('Noble bindings central', function() {
-  var sandbox;
+var Abstract = require('./test-bindings-abstract');
+
+
+//native doesnt need to do any setup on these
+Abstract.startScanningEmitScanStart(bindings, Mock);
+Abstract.stopScanningEmitScanStop(bindings, Mock);
+
+//we need to stub/emit something for these
+Abstract.emitAddressChange(bindings, Mock, function(mock)
+{
+  mock.mockCentral.emit('address', a.addressString);
+});
+
+Abstract.emitStateChange(bindings, Mock, function(mock){
+  mock.mockCentral.emit('stateUpdate', a.stateString);
+});
+
+Abstract.emitDiscover(bindings, Mock, function(mock){
+  mock.mockCentral.emit('peripheralDiscover', mock.nativePeripheralObject, mock.nativeAdvertisementObject, a.rssiNumber);
+});
+
+Abstract.emitConnectSuccess(bindings, Mock, function(mock){
+  mock.mockCentral.emit('peripheralConnect', mock.nativePeripheralObject, a.mockError);
+});
+
+Abstract.emitConnectFail(bindings, Mock, function(mock){
+  mock.mockCentral.emit('peripheralConnectFail', mock.nativePeripheralObject, a.mockError);
+});
+
+Abstract.emitDisconnect(bindings, Mock, function(mock, sandbox){
+
+  sandbox.stub(mock.nativePeripheralObject, "cancelConnection", function(){
+    mock.mockCentral
+    .emit('peripheralDisconnect', mock.nativePeripheralObject);
+  });
+
+  //set internal state
+  mock.discoverPeripheral();
+  mock.discoverServices();
+  mock.discoverCharacteristics();
+  mock.discoverDescriptors();
+
+});
+
+
+
+
+    
+
+
+describe('Core Bluetooth Bindings Central', function() {
+  var sandbox = sinon.sandbox.create();
   var mock;
 
   beforeEach(function() {
-    sandbox = sinon.sandbox.create();
     mock = new Mock(bindings, sandbox);
   });
 
@@ -28,15 +77,6 @@ describe('Noble bindings central', function() {
     calledSpy.calledWithExactly(a.serviceUuidsArray, a.allowDuplicates).should.equal(true);
   });
 
-  it('startScanning should emit scanStart', function() {
-    var eventSpy = sandbox.spy();
-    bindings.on('scanStart', eventSpy);
-
-    bindings.startScanning();
-
-    eventSpy.called.should.equal(true);
-  });
-
   it('stopScanning should call native', function() {
     var calledSpy = sandbox.spy(mock.mockCentral, 'stopScan');
 
@@ -44,83 +84,4 @@ describe('Noble bindings central', function() {
 
     calledSpy.called.should.equal(true);
   });
-
-  it('stopScanning should emit scanStop', function() {
-    var eventSpy = sandbox.spy();
-    bindings.on('scanStop', eventSpy);
-
-    bindings.stopScanning();
-
-    eventSpy.called.should.equal(true);
-  });
-
-  it('should emit addressChange', function() {
-    var eventSpy = sandbox.spy();
-    bindings.once('addressChange', eventSpy);
-
-    mock.mockCentral.emit('address', a.addressString);
-
-    eventSpy.calledWithExactly(a.addressString).should.equal(true);
-  });
-
-
-  it('should emit stateChange', function() {
-    var eventSpy = sandbox.spy();
-    bindings.once('stateChange', eventSpy);
-
-    mock.mockCentral.emit('stateUpdate', a.stateString);
-
-    eventSpy.calledWithExactly(a.stateString).should.equal(true);
-  });
-
-  it('should emit discover', function() {
-    var eventSpy = sandbox.spy();
-    bindings.once('discover', eventSpy);
-
-    mock.mockCentral.emit('peripheralDiscover', mock.nativePeripheralObject, mock.nativeAdvertisementObject, a.rssiNumber);
-
-    eventSpy.calledWithExactly(a.peripheralUuidString, a.addressString, a.addressTypeString, a.connectableBoolean, a.advertisementObject, a.rssiNumber).should.equal(true);
-  });
-
-  it('should emit connect on success', function() {
-    var eventSpy = sandbox.spy();
-    bindings.once('connect', eventSpy);
-
-    mock.mockCentral.emit('peripheralConnect', mock.nativePeripheralObject, a.mockError);
-
-    eventSpy.calledWithExactly(a.peripheralUuidString, a.mockError).should.equal(true);
-  });
-
-  it('should emit connect on fail', function() {
-    var eventSpy = sandbox.spy();
-    bindings.once('connect', eventSpy);
-
-    mock.mockCentral.emit('peripheralConnectFail', mock.nativePeripheralObject, a.mockError);
-
-    eventSpy.calledWithExactly(a.peripheralUuidString, a.mockError).should.equal(true);
-  });
-
-  it('should emit disconnect', function() {
-    var eventSpy = sandbox.spy();
-    bindings.once('disconnect', eventSpy);
-
-    //stub native
-    sandbox.stub(mock.nativePeripheralObject, "cancelConnection", function(){
-      mock.mockCentral
-      .emit('peripheralDisconnect', mock.nativePeripheralObject);
-    });
-
-    //set internal state
-    mock.discoverPeripheral();
-    mock.discoverServices();
-    mock.discoverCharacteristics();
-    mock.discoverDescriptors();
-
-    //make the call
-    bindings.disconnect(a.peripheralUuidString);
-
-    eventSpy.calledWithExactly(a.peripheralUuidString).should.equal(true);
-  });
-
-
 });
