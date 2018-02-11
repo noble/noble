3cfb558c9f31aa9e1e5690f036fd2efcaf208d8e
Hadrien Kohl <hadrien.kohl@gmail.com>
2018-02-11 22:07:35 +0100

    Websocket bugfix (#750)
    * Close connection on error'
    
    * Log errors when sending messages to websocket
    
    * Send poweredOn when connecting and forward state changes
    
    * Make sure the event handler are setup before sending poweredOn

--- ws-slave.js
+++ ws-slave.js
@@ -25,18 +25,29 @@ if (serverMode) {
 
     ws = ws_;
 
-    sendEvent({
-      type: 'stateChange',
-      state: noble.state
-    });
-
     ws.on('message', onMessage);
 
     ws.on('close', function() {
       console.log('ws -> close');
-
       noble.stopScanning();
     });
+
+    noble.on('stateChange', function(state) {
+      sendEvent({
+        type: 'stateChange',
+        state: state
+      });
+    });
+
+    // Send poweredOn if already in this state.
+    if (noble.state == "poweredOn") {
+      sendEvent({
+        type: 'stateChange',
+        state: "poweredOn"
+      });
+    }
+
+
   });
 } else {
   ws = new WebSocket('ws://' + host + ':' + port);
